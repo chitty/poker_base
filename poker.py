dfbb3968d2844191f88b371bfd89ad097ee0eb77
Carlos Chitty <chitty.carlosj@gmail.com>
2016-06-17 18:28:06 -0400

    PEP8

--- poker.py
+++ poker.py
@@ -1,19 +1,19 @@
 def card_ranks(cards):
     """Return a list of the ranks, sorted with higher first.
 
-	Args:
-		hand: list of 5 2-letter strings representing card rank and suit.
+    Args:
+        hand: list of 5 2-letter strings representing card rank and suit.
     """
-    ranks = ['--23456789TJQKA'.index(r) for r,s in cards]
+    ranks = ['--23456789TJQKA'.index(r) for r, s in cards]
     ranks.sort(reverse=True)
     return [5, 4, 3, 2, 1] if (ranks == [14, 5, 4, 3, 2]) else ranks
 
 
 def straight(ranks):
     """Return True if the ordered ranks form a 5-card straight.
-    
-	Args:
-		ranks: list of ranks of cards in descending order.
+
+    Args:
+        ranks: list of ranks of cards in descending order.
     """
     return (max(ranks)-min(ranks) == 4) and len(set(ranks)) == 5
 
@@ -21,40 +21,40 @@ def straight(ranks):
 def flush(hand):
     """"Return True if all the cards have the same suit.
 
-	Args:
-		hand: list of 5 2-letter strings representing card rank and suit.
+    Args:
+        hand: list of 5 2-letter strings representing card rank and suit.
     """
     return hand[0][1] == hand[1][1] == hand[2][1] == hand[3][1] == hand[4][1]
 
 
 def kind(n, ranks):
-	"""
-	Return the first rank that this hand has exactly n of.
-	Return None if there is no n-of-a-kind in the hand.
+    """
+    Return the first rank that this hand has exactly n of.
+    Return None if there is no n-of-a-kind in the hand.
 
-	Args:
-		n: number of equal ranks to be checked. Valid values: 1 to 4.
-		ranks: list of ranks of cards in descending order.
-	"""
-	for r in ranks:
-		if ranks.count(r) == n:
-			return r
-	return None
+    Args:
+        n: number of equal ranks to be checked. Valid values: 1 to 4.
+        ranks: list of ranks of cards in descending order.
+    """
+    for r in ranks:
+        if ranks.count(r) == n:
+            return r
+    return None
 
 
 def two_pair(ranks):
-	"""If there are two pair, return the two ranks as a
-	tuple: (highest, lowest); otherwise return None.
+    """If there are two pair, return the two ranks as a
+    tuple: (highest, lowest); otherwise return None.
 
-	Args:
-		ranks: list of ranks of cards in descending order.
-	"""
-	pair = kind(2, ranks)
-	lowpair = kind(2, list(reversed(ranks)))
-	if pair and lowpair != pair:
-		return (pair, lowpair)
-	else:
-		return None
+    Args:
+        ranks: list of ranks of cards in descending order.
+    """
+    pair = kind(2, ranks)
+    lowpair = kind(2, list(reversed(ranks)))
+    if pair and lowpair != pair:
+        return (pair, lowpair)
+    else:
+        return None
 
 
 def hand_rank(hand):
@@ -80,27 +80,27 @@ def hand_rank(hand):
 
 
 def allmax(iterable, key=None):
-	"""Return a list of all items equal to the max of the iterable.
+    """Return a list of all items equal to the max of the iterable.
 
-	Args:
-		iterable: list of all the items, poker hands.
-		key: method to do comparisons.
-	"""
-	result, maxval = [], None
-	key = key or (lambda x: x)
-	for x in iterable:
-		xval = key(x)
-		if not result or xval > maxval:
-			result, maxval = [x], xval
-		elif xval == maxval:
-			result.append(x)
-	return result
+    Args:
+        iterable: list of all the items, poker hands.
+        key: method to do comparisons.
+    """
+    result, maxval = [], None
+    key = key or (lambda x: x)
+    for x in iterable:
+        xval = key(x)
+        if not result or xval > maxval:
+            result, maxval = [x], xval
+        elif xval == maxval:
+            result.append(x)
+    return result
 
 
 def poker(hands):
     """Return a list of winning hands: poker([hand,...]) => [hand,...]
 
-	Args:
-		hands: list of hands to compare.
+    Args:
+        hands: list of hands to compare.
     """
     return allmax(hands, key=hand_rank)
--- test.py
+++ test.py
@@ -1,24 +1,25 @@
 from poker import poker, kind, two_pair, hand_rank, card_ranks
 
+
 def test():
     "Test cases for the functions in poker program"
-    sf = "6C 7C 8C 9C TC".split() # Straight Flush
-    fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
-    fh = "TD TC TH 7C 7D".split() # Full House
-    tp = "5S 5D AC AS KS".split() # Two Pair
-    s1 = "AC 2S 3C 4D 5D".split() # A-5 Straight 
-    s2 = "2S 3C 4D 5D 6S".split() # 2-6 Straight
-    ah = "AC 2S 9C 4D 6D".split() # A High 
-    sh = "7C 2S 6C 3D 5D".split() # 7 High
+    sf = "6C 7C 8C 9C TC".split()  # Straight Flush
+    fk = "9D 9H 9S 9C 7D".split()  # Four of a Kind
+    fh = "TD TC TH 7C 7D".split()  # Full House
+    tp = "5S 5D AC AS KS".split()  # Two Pair
+    s1 = "AC 2S 3C 4D 5D".split()  # A-5 Straight
+    s2 = "2S 3C 4D 5D 6S".split()  # 2-6 Straight
+    ah = "AC 2S 9C 4D 6D".split()  # A High
+    sh = "7C 2S 6C 3D 5D".split()  # 7 High
     assert poker([s1, s2, ah, sh]) == [s2]
     assert poker([s1, ah, sh]) == [s1]
     fkranks = card_ranks(fk)
     tpranks = card_ranks(tp)
     assert kind(4, fkranks) == 9
-    assert kind(3, fkranks) == None
-    assert kind(2, fkranks) == None
+    assert kind(3, fkranks) is None
+    assert kind(2, fkranks) is None
     assert kind(1, fkranks) == 7
-    assert two_pair(fkranks) == None
+    assert two_pair(fkranks) is None
     assert two_pair(tpranks) == (14, 5)
     assert poker([sf, fk, fh]) == [sf]
     assert poker([fk, fh]) == [fk]
