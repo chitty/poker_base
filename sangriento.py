6e7d28f500caa32172f0bd57716c8e006eaf2f92
Carlos Chitty <chitty.carlosj@gmail.com>
2016-07-11 12:57:42 -0400

    Added the simple sangriento game.

---
+++ sangriento.py
@@ -0,0 +1,31 @@
+from poker import poker, hand_rank
+from deal import deal
+
+hand_names = ["High Card", "Pair", "Two Pair", "3 of a Kind", "Straight",
+              "Flush", "Full House", "4 of a Kind", "Straight Flush"]
+
+def sangriento(n):
+    hands = deal(n)
+    for hand in hands:
+        print hand
+
+    winner = poker(hands)
+    ranking = hand_rank(winner[0])[0]
+
+    print "Winner hand(s):"
+    print hand_names[ranking]
+
+    return winner
+
+
+players = 0
+while players < 2 or players > 10:
+    try:
+        players = int(raw_input("Enter number of players (2-10): ").strip())
+        if players > 1 and players < 11:
+            print sangriento(players)
+        else:
+            print "Minimunt players: 2. Maximum: 10."
+    except:
+        print "Invalid input"
+    
